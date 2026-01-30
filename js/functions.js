206d5de2e58f60ba88d912ac859190e99b3f8d1f
MetalArt2025 <info@metalart.nl>
2026-01-30 13:33:46 +0100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -235,7 +235,7 @@ materialsArr = [
         maxDiameter: [25, 38],
         cutWidth: [2, 2],
         finishPrice: [20, 15],
-        ppmm: [1, 2.8],
+        ppmm: [0.96, 3],
         startCost: [20, 10],
         customerFactor: [9.2, 8],
         margin: [
@@ -248,7 +248,7 @@ materialsArr = [
         maxDiameter: [25, 38],
         cutWidth: [2, 2],
         finishPrice: [30, 25],
-        ppmm: [1.1, 2.8],
+        ppmm: [1, 3],
         startCost: [20, 10],
         customerFactor: [9.2, 8],
         margin: [
