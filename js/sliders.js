221890c1e5dca875c19b3c83b19f1778aaadfb22
MetalArt2025 <info@metalart.nl>
2026-04-01 17:27:09 +0200

    Update sliders.js

--- js/sliders.js
+++ js/sliders.js
@@ -394,7 +394,7 @@ function replaceSliders(sliders) {
 
 updateAffected = function () {
   currentRingValid = 1;
-  var maxOuterDiameter = [31, 37, 29, 38, 32];
+  var maxOuterDiameter = [31, 37, 29, 37, 32];
   max = maxOuterDiameter[getMaterialIndex()] / 2;
   if (ringObj.totalHeight > max) {
     currentRingValid = 0;
